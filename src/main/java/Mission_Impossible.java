0718402ad1f74ebf5941cd4552620b839e1f432b
Omar El-Meteny <omar.elmeteny@gmail.com>
2023-01-03 22:44:43 +0200

    add test cases

--- src/main/java/Mission_Impossible.java
+++ src/main/java/Mission_Impossible.java
@@ -4,78 +4,120 @@
 public class Mission_Impossible {
 
     public static int alpha(int[] keys) {
-        IndexSubrange largest = LargestEvenSubrange(keys, 0, keys.length - 1);
-        reverseSubrange(keys, largest);
-        return SumRange(keys, 0, keys.length - 1, true);
+        Range largest = largestSubrange(keys, 0, keys.length - 1);
+        if (shouldReverse(largest))
+        {
+            reverseSubrange(keys, largest);
+        }
+        return sumEven(keys);
     }
 
-    public static void reverseSubrange(int[] keys, IndexSubrange range) {
-        for (int i = range.start; i < (range.start + range.end + 1) / 2; i++) {
+    // Check if the subrange should be reversed
+    // If the subrange's value is greater than zero // (i.e. the alpha output is increased)
+    public static boolean shouldReverse(Range range) {
+        return range.value > 0;
+    }
+
+    // Revese a subrange of an array
+    public static void reverseSubrange(int[] keys, Range range) {
+        for (int i = range.lo; i < (range.lo + range.hi + 1) / 2; i++) {
             int temp = keys[i];
-            keys[i] = keys[range.end + range.start - i];
-            keys[range.end + range.start - i] = temp;
+            keys[i] = keys[range.hi + range.lo - i];
+            keys[range.hi + range.lo - i] = temp;
         }
     }
 
-    public static IndexSubrange LargestEvenSubrange(int[] a, int lo, int hi) {
+    // A variant of a largestSubrange algorithm 
+    // instead of returning the max value as in the lecture
+    // returns an object holding the max value 
+    // and the lo and hi indices of range corresponding to the maximum value
+    public static Range largestSubrange(int[] a, int lo, int hi) {
         if (lo == hi) {
-            return new IndexSubrange(lo, lo, a[lo]);
+            return new Range(lo, lo, 0);
         }
         int mid = (lo + hi) / 2;
-        IndexSubrange left = LargestEvenSubrange(a, lo, mid);
-        IndexSubrange right = LargestEvenSubrange(a, mid + 1, hi);
-        IndexSubrange maxCrossing = MaximumCrossing(a, lo, hi, mid);
 
-        if (left.sum > right.sum) {
-            return left.sum > maxCrossing.sum ? left : maxCrossing;
+        Range left = largestSubrange(a, lo, mid);
+        Range right = largestSubrange(a, mid + 1, hi);
+        Range maxCrossing = maximumCrossing(a, lo, hi, mid);
+
+        if (left.value > right.value) {
+            return left.value > maxCrossing.value ? left : maxCrossing;
         } else {
-            return right.sum > maxCrossing.sum ? right : maxCrossing;
+            return right.value > maxCrossing.value ? right : maxCrossing;
         }
     }
 
-    public static int SumRange(int[] a, int lo, int hi, boolean evenPositions) {
-        if (lo % 2 == (evenPositions ? 1 : 0)) {
-            lo++;
-        }
+    // Calculate the output of alpha (the sum of values at even positions)
+    public static int sumEven(int[] keys) {
         int sum = 0;
-        for (int i = lo; i <= hi; i += 2) {
-            sum += a[i];
+        for (int i = 0; i < keys.length; i += 2) {
+            sum += keys[i];
         }
         return sum;
     }
 
-    public static IndexSubrange MaximumCrossing(int[] a, int lo, int hi, int mid) {
-        int sumLeft = SumRange(a, lo, mid, false);
-        int sumRight = SumRange(a, mid + 1, hi, false);
-        int sum = sumLeft + sumRight;
+    // Calculate the crossing value
+    // This is the increase or decrease of alpha output
+    // when the array subrange is reversed
+    //
+    // When the number of elements to be reversed is odd: return 0
+    // since reversing odd number of keys will cause
+    // odd position keys to be still at odd positions after reversing
+    // 
+    // When the number of items to be reversed is even:
+    // if i is odd, keys[i] will be at an even position after reversing so increment sum by key[i]
+    // if i is even, keys[i] will be at an odd position after reversing so decrement sum by key[i]
+    public static int crossingValue(int[] keys, int lo, int hi) {
+        if ((lo - hi) % 2 == 0) {
+            return 0;
+        }
+        int sum = 0;
+        for (int i = lo; i <= hi; i ++) {
+            sum += (i % 2 == 1 ? 1 : -1 ) * keys[i];
+        }
+        return sum;
+    }
 
-        if (sumLeft > sumRight) {
-            return sumLeft > sum ? new IndexSubrange(lo, mid, sumLeft) : new IndexSubrange(lo, hi, sum);
+    // calculate the maximim crossing in a range
+    // This is variant of the maximumCrossing function of the largest subrange alg. discussed in the lectures
+    // Instead of returning the value return an object containing the indices and the value
+    public static Range maximumCrossing(int[] keys, int lo, int hi, int mid) {
+        if (lo == hi) {
+            return new Range(lo, hi, 0);
+        }
+
+        int midLeft = (mid - lo) % 2 == 0 ? mid + 1 : mid;
+        int midRight = (hi - mid) % 2 == 0 ? mid - 1 : mid;
+        
+        int valueLeft = crossingValue(keys, lo, midLeft);
+        int valueRight = crossingValue(keys, midRight, hi);
+        // We can't add the values of valueLeft and valueRight since they may overlap
+        int value = crossingValue(keys, lo, hi);
+        
+        if (valueLeft > valueRight) {
+            return valueLeft > value ? new Range(lo, midLeft, valueLeft) : new Range(lo, hi, value);
         } else {
-            return sumRight > sum ? new IndexSubrange(mid + 1, hi, sumRight) : new IndexSubrange(lo, hi, sum);
+            return valueRight > value ? new Range(midRight, hi, valueRight) : new Range(lo, hi, value);
         }
     }
 
-    // public static int max(int x, int y, int z) {
-    // return z > (x > y ? x : y) ? z : ((x > y) ? x : y);
-    // }
-
     public static void main(String[] args) {
         // int[] keys = {1,7,3,4,7,6,2,9};
-        int[] keys = {3,1,2,1};
+        int[] keys = {1,7,3,4,7,6,2,9};
         System.out.println(alpha(keys));
     }
 
 }
 
-class IndexSubrange {
-    int start;
-    int end;
-    int sum;
+class Range {
+    int lo;
+    int hi;
+    int value;
 
-    IndexSubrange(int start, int end, int sum) {
-        this.start = start;
-        this.end = end;
-        this.sum = sum;
+    Range(int start, int end, int value) {
+        this.lo = start;
+        this.hi = end;
+        this.value = value;
     }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,20 +1,175 @@
 
-
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
+    TestValues[] testValues = {
+            // new TestValues(new int[] { 1, 2 }, true, 0, 1, 2),
+            // new TestValues(new int[] { 2, 1 }, false, 0, 1, 2),
+            // new TestValues(new int[] { 1, 2, 3 }, true, 0, 1, 5),
+            // new TestValues(new int[] { 1, 3, 2 }, true, 0, 1, 5),
+            // new TestValues(new int[] { 3, 1, 2 }, false, 0, 1, 5),
+            // new TestValues(new int[] { 3, 2, 1 }, true, 1, 2, 5),
+            // new TestValues(new int[] { 1, 2, 3, 4 }, true, 0, 3, 6),
+            // new TestValues(new int[] { 1, 2, 4, 3 }, true, 0, 1, 6),
+            // new TestValues(new int[] { 1, 3, 2, 4 }, true, 0, 3, 7),
+            // new TestValues(new int[] { 1, 3, 4, 2 }, true, 0, 1, 7),
+            new TestValues(new int[] { 3 }, false, 0, 0, 3),
+            new TestValues(new int[] { 3, 1, 2, 1 }, false, 0, 3, 5),
+            new TestValues(new int[] { 1, 7, 3, 4, 7, 6, 2, 9 }, false, 0, 3, 26),
+            new TestValues(new int[] { 2, 1, 5, 7, 1, 1 }, true, 3, 4, 14),
+            new TestValues(new int[] { 2, 4, 1, 7, 1, 1 }, true, 1, 4, 13),
+            new TestValues(new int[] { 5, 6, 6, 5, 5, 6 }, true, 0, 1, 17),
+            new TestValues(new int[] { 1, 2, 3, 4, 8, 7, 5, 6 }, true, 0, 7, 19),
+            new TestValues(new int[] { 1, 2, 3, 4, 8, 7, 6, 5 }, true, 0, 3, 20),
+            new TestValues(new int[] { 1, 2, 3, 4, 5, 6, 8, 7 }, true, 6, 7, 20),
+            new TestValues(new int[] { 1, 2, 3, 4, 5, 7, 8, 6 }, true, 0, 5, 21),
+
+
+    };
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldSucceedTest0() {
+        runTest(0);
+    }
+
+    @Test
+    public void shouldSucceedTest1() {
+        runTest(1);
+    }
+
+    @Test
+    public void shouldSucceedTest2() {
+        runTest(2);
+    }
+
+    @Test
+    public void shouldSucceedTest3() {
+        runTest(3);
+    }
+
+    @Test
+    public void shouldSucceedTest4() {
+        runTest(4);
+    }
+
+    @Test
+    public void shouldSucceedTest5() {
+        runTest(5);
+    }
+
+    @Test
+    public void shouldSucceedTest6() {
+        runTest(6);
+    }
+
+    @Test
+    public void shouldSucceedTest7() {
+        runTest(7);
+    }
+
+    @Test
+    public void shouldSucceedTest8() {
+        runTest(8);
+    }
+
+    @Test
+    public void shouldSucceedTest9() {
+        runTest(9);
+    }
+
+    private void runTest(int i) {
+        TestValues testValue = testValues[i];
+        // Range range = Mission_Impossible.largestSubrange(testValue.testArray, 0,
+        //         testValue.testArray.length - 1);
+        // boolean shouldReverse = Mission_Impossible.shouldReverse(range);
+        // assertEquals(String.format("test %d failed: shouldReverse ", i), testValue.shouldReverse, shouldReverse);
+        // if (shouldReverse) {
+        //     assertEquals(String.format("test %d failed: range lo", i), testValue.lo, range.lo);
+        //     assertEquals(String.format("test %d failed: range hi", i), testValue.hi, range.hi);
+        // }
+        int output = Mission_Impossible.alpha(testValue.testArray);
+        assertEquals(String.format("failed %d failed: alpha output", i), testValue.output, output);
+    }
+
+    @Test
+    public void shouldReverseOddArrayCorrectly() {
+        int[] oddArray = { 1, 2, 3, 4, 5};
+
+        Mission_Impossible.reverseSubrange(oddArray, new Range(0, 4, 0));
+        assertEquals(5, oddArray[0]);
+        assertEquals(4, oddArray[1]);
+        assertEquals(3, oddArray[2]);
+        assertEquals(2, oddArray[3]);
+        assertEquals(1, oddArray[4]);
+    }
+
+    @Test
+    public void shouldReverseEvenArrayCorrectly() {
+        int[] oddArray = { 1, 2, 3, 4 };
+
+        Mission_Impossible.reverseSubrange(oddArray, new Range(0, 3, 0));
+        assertEquals(4, oddArray[0]);
+        assertEquals(3, oddArray[1]);
+        assertEquals(2, oddArray[2]);
+        assertEquals(1, oddArray[3]);
+    }
+
+    @Test
+    public void shouldReverseEvenArrayOddSubRangeCorrectly() {
+        int[] oddArray = { 1, 2, 3, 4, 5, 6 };
+
+        Mission_Impossible.reverseSubrange(oddArray, new Range(1, 3, 0));
+        assertEquals(1, oddArray[0]);
+        assertEquals(4, oddArray[1]);
+        assertEquals(3, oddArray[2]);
+        assertEquals(2, oddArray[3]);
+        assertEquals(5, oddArray[4]);
+        assertEquals(6, oddArray[5]);
+        
+    }
+
+    @Test
+    public void shouldReverseEvenArrayEvenSubRangeCorrectly() {
+        int[] oddArray = { 1, 2, 3, 4, 5, 6 };
+
+        Mission_Impossible.reverseSubrange(oddArray, new Range(1, 4, 0));
+        assertEquals(1, oddArray[0]);
+        assertEquals(5, oddArray[1]);
+        assertEquals(4, oddArray[2]);
+        assertEquals(3, oddArray[3]);
+        assertEquals(2, oddArray[4]);
+        assertEquals(6, oddArray[5]);
+        
+    }
+
+    @Test
+    public void shouldCalculateRangeCorrectly() {
+        int[] a = new int[] { 1, 10, 100, 1000, 10000, 100000 };
+        assertEquals(0, Mission_Impossible.crossingValue(a, 0, 0));
+        assertEquals(9, Mission_Impossible.crossingValue(a, 0, 1));
+        assertEquals(a[1] + a[3] + a[5] - a[0] - a[2] - a[4], Mission_Impossible.crossingValue(a, 0, 5));
+        assertEquals(a[3] - a[2], Mission_Impossible.crossingValue(a, 2, 3));
+    }
+}
+
+class TestValues {
+    int[] testArray;
+    boolean shouldReverse;
+    int lo;
+    int hi;
+    int output;
+
+    public TestValues(int[] testArray, boolean shouldReverse, int lo, int hi, int output) {
+        this.testArray = testArray;
+        this.shouldReverse = shouldReverse;
+        this.lo = lo;
+        this.hi = hi;
+        this.output = output;
     }
 }
